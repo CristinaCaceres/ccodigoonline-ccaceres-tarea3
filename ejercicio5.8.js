0046404b1c650e04fe85a49020ac72a686816b8e
CCaceres <cristina.caceres.jauregui@gmail.com>
2020-05-01 18:22:54 -0500

    Tarea - terminando Ejercicio 5.8

--- ejercicio5.8.js
+++ ejercicio5.8.js
@@ -4,7 +4,23 @@
 // el segundo pase a ser el tercero, el último pase a ser el primero, y así
 // sucesivamente. Represéntelo mediante un diagrama de flujo.
 
-let vector1 = [];
+let vector1 = [2,4,5,8,7,3,2,1,5];
 let vector2 = [];
 let i;
 
+for(i=0;i<vector1.length;i++){
+    
+    if(i===vector1.length-1){
+        vector2[0] = vector1[i]; 
+    }
+    else{
+        vector2[i+1] = vector1[i]; 
+    }
+}
+
+let j=0;
+while(j<vector2.length){
+    console.log(`Vector 2 posicion ${j} valor : ${vector2[j]}`);
+    
+    j++;
+}
